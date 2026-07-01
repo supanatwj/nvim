ec10cb06ab8a926a19e3840c1ce96bee6f03a8f0
supanatwj <supanat.wji@gmail.com>
2026-07-01 23:53:05 +0700

    lsp: install lsp

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,27 @@
+return {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        --list of server for mason to install
+        ensure_installed = {
+            "ts_ls",
+            "html",
+            "cssls",
+            "lua_ls",
+            "ts_ls",
+            "eslint",
+        },
+     },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        } 
+    },
+        "neovim/nvim-lspconfig",
+    },
+}
