eed051834b6b562ec158fc923aeb421cf2e02919
supanatwj <supanat.wji@gmail.com>
2026-07-02 13:20:27 +0700

    change plugin for cmp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,3 +34,5 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+build = function() require('blink.cmp').build():pwait() end
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,60 +1,63 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
     {
-      "L3MON4D3/LuaSnip",
-      -- follow latest release.
-      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
-      build = "make install_jsregexp",
-    },
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-  },
-  config = function()
-    local cmp = require("cmp")
-
-    local luasnip = require("luasnip")
-
-    local lspkind = require("lspkind")
-
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    cmp.setup({
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
+        'saghen/blink.cmp',
+        dependencies = {
+            'saghen/blink.lib',
+            -- optional: provides snippets for the snippet source
+            'rafamadriz/friendly-snippets',
+        },
+        build = function()
+            -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
+            -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
+            require('blink.cmp').build():pwait()
         end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      }),
-      -- sources for autocompletion
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+            -- 'super-tab' for mappings similar to vscode (tab to accept)
+            -- 'enter' for enter to accept
+            -- 'none' for no mappings
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-n/C-p or Up/Down: Select next/previous item
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help (if signature.enabled = true)
+            --
+            -- See :h blink-cmp-config-keymap for defining your own keymap
+            keymap = {
+                preset = 'default',
 
-      -- configure lspkind for vs-code like pictograms in completion menu
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
-      },
-    })
-  end,
+                -- String returns - feedkeys() with 't' flag (remap enabled)
+                -- User mappings take precedence over built-in behavior
+                ['<C-n>'] = { 'select_next' },
+                ['<C-j>'] = { function(cmp) return '<C-n>' end },
+
+                 -- Chained commands
+                ['<C-p>'] = { 'select_prev', 'fallback' },
+                ['<C-K>'] = { function(cmp) return '<C-p>' end },
+
+                -- Multi-key sequences
+                ['<C-x><C-o>'] = { 'show', 'fallback' },
+                ['jk'] = { 'hide' },
+
+                -- Key equivalences (for terminals that support them)
+                ['<C-i>'] = { 'accept', 'snippet_forward', 'fallback' },
+                ['<Tab>'] = { 'select_next', 'fallback' },
+
+                -- Override preset key
+                ['<C-y>'] = { 'select_and_accept' },
+
+                -- Disable preset key
+                ['<C-e>'] = false, -- or {}
+
+                -- Function calling blink.cmp method
+                ['<C-Space>'] = { 'show' }, -- This is equivalent as above
+
+                -- Actions with parameters require functions
+                ['<C-space>S'] = { function(cmp) return cmp.show({ providers = { 'snippets' } }) end },
+           },
+            -- (Default) Only show the documentation popup when manually triggered
+            completion = { documentation = { auto_show = false } },
+       },
+    }
 }
