dd6e45c64338c9d191198d2dcaab4c5ba444b58d
Supanat <gincrol34@gmail.com>
2026-09-08 21:31:51 +0700

    background color

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,21 @@
 function ColorMyPencils(color)
     color = color;
-    -- vim.cmd.colorscheme(color)
+    vim.cmd.colorscheme(color)
+
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 end
 
-ColorMyPencils()
+return {
+    -- the colorscheme should be available when starting Neovim
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        opts = {},
+        config = function()
+            vim.cmd [[colorscheme tokyonight]]
+            ColorMyPencils()
+        end
+    },
+}
--- lua/plugins/examples.lua
+++ lua/plugins/examples.lua
@@ -1,16 +1,4 @@
 return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-        transparent = true,
-        config = function ()
-            vim.cmd([[colorscheme tokyonight-night]])
-        end,
-    },
-  },
 
   -- I have a separate config.mappings file where I require which-key.
   -- With lazy the plugin will be automatically loaded when it is required somewhere
